8044c79373d39550cecc24a6e57c20374fbf4513
LinLinHayashi <140152128+LinLinHayashi@users.noreply.github.com>
2023-12-04 01:12:14 -0800

    update listing API route created

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -30,3 +30,23 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found!'));
+  }
+  if (req.user.id !== listing.userRef){
+    return next(errorHandler(401, 'You can only update your own listings!'));
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {new: true} // This will return the new updated object rather than the old object.
+    );
+    res.status(200).json(updatedListing);
+  } catch (error){
+    next(error);
+  }
+};
