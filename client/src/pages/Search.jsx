f39ac4d256eda419bda79f553f444b1bd6f71e64
LinLinHayashi <140152128+LinLinHayashi@users.noreply.github.com>
2023-12-20 02:55:42 -0800

    show more listings functionality added

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -14,10 +14,9 @@ export default function Search() {
   });
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
+  const [showMore, setShowMore] = useState(false);
 
   const navigate = useNavigate();
-
-  console.log(listings);
 
   useEffect(() => {
 
@@ -52,9 +51,13 @@ export default function Search() {
 
     const fetchListings = async () => {
       setLoading(true);
+      setShowMore(false);
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
-      const data = await res.json();
+      const data = await res.json(); // Note that "data" is an array of listings.
+      if (data.length > 8){
+        setShowMore(true);
+      }
       setListings(data);
       setLoading(false);
     };
@@ -92,6 +95,20 @@ export default function Search() {
     urlParams.set('order', sidebardata.order);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
+  };
+
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res =  await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9){
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
   };
 
   return (
@@ -156,6 +173,9 @@ export default function Search() {
           {!loading && listings && listings.map(listing => (
             <ListingItem key={listing._id} listing={listing} />
           ))}
+          {showMore && (
+            <button onClick={onShowMoreClick} className='text-green-700 hover:underline p-7 text-center w-full'>Show more</button>
+          )}
         </div>
       </div>
     </div>
