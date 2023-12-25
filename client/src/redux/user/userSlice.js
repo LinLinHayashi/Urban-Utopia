a81a291d4cf307fe6eddbb05654b17e7cb20e92d
LinLinHayashi <140152128+LinLinHayashi@users.noreply.github.com>
2023-12-25 15:18:28 -0800

    comments about redux updated

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -16,8 +16,8 @@ const userSlice = createSlice({
     signInStart: (state) => {
       state.loading = true;
     },
-    signInSuccess: (state, action) => { // Actions are JS objects.
-      state.currentUser = action.payload; // "payload" is an optional attribute of an action, which is the value passed with the action. Another action attribute is "type".
+    signInSuccess: (state, action) => { // Action is a JS object.
+      state.currentUser = action.payload; // "payload" is an attribute of an action, which is the value passed with the action. Another action attribute is "type".
       state.loading = false;
       state.error = null;
     },
@@ -64,15 +64,16 @@ const userSlice = createSlice({
   }
 });
 
-/* Action creators and reducers:
-1. ".actions" attribute of "userSlice" is an automatically generated object that contains action creators.
-2. Action creators have the same names as the reducers and can be used to dispatch actions with the corresponding action types.
-3. "userSlice.actions" will look like this:
-   {
-    signInStart: createAction('user/signInStart'),
-    signInSuccess: createAction('user/signInSuccess'),
-    signInFailure: createAction('user/signInFailure)
-   }
+/*
+  Action creators and reducers:
+  1. ".actions" attribute of "userSlice" is an automatically generated object that contains action creators.
+  2. Action creators have the same names as the reducers and can be used to dispatch actions with the corresponding action types.
+  3. "userSlice.actions" will look like this:
+    {
+      signInStart: createAction('user/signInStart'),
+      signInSuccess: createAction('user/signInSuccess'),
+      signInFailure: createAction('user/signInFailure)
+    }
 */
 export const {
   signInStart, signInSuccess, signInFailure,
@@ -81,4 +82,5 @@ export const {
   signOutUserStart, signOutUserSuccess, signOutUserFailure
 } = userSlice.actions; // Each of these variables is an action creator now.
 
+// We only need to import this in "store.js" file to hook this slice with the store.
 export default userSlice.reducer; // "default" means we can change its name when importing it in other files. 
